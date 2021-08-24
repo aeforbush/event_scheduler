3e2b4918c1c28e1a0760690f0542b79437cfbef8
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-24 11:14:16 -0600

    added date

---
+++ super-disco-main/Develop/script.js
@@ -0,0 +1,17 @@
+// VARIABLES
+
+
+// when opened display current month, day, year
+$("#currentDay").text(moment().format('MM/DD/YY'));
+console.log(currentDay);
+
+
+// ability to add tasks
+
+// create task function
+
+// save task function
+
+
+
+
