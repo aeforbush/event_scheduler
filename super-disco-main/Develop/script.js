4e8e8ee23a8fa667c172d54adb47e8e76d934308
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-24 13:24:39 -0600

    added time sensitive color coding to input

--- super-disco-main/Develop/script.js
+++ super-disco-main/Develop/script.js
@@ -15,12 +15,30 @@ $(document).ready(function() {
 function colorChange() {
     var currentTime = moment().hours();
     console.log(currentTime);
-};
 // function that tells blocks whether they are past, present or future
 $(".input").each(function() {
+    // fetching an id attribute and parsing it into an integer
     var scheduledTime = parseInt($(this).attr("id"));
     console.log(scheduledTime);
+
+    // check for past currentTime
+    if(currentTime > scheduledTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+        // check for future of currentTime
+    } else if (currentTime < scheduledTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+        // check if present time is equal to currentTime
+    } else {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
 })
+};
 
 // ability to add tasks
 function renderText() {
