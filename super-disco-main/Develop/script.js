76e27002f30bb376e6b37bde0a1513dc52830c32
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-24 11:49:21 -0600

    adding color code to blocks

--- super-disco-main/Develop/script.js
+++ super-disco-main/Develop/script.js
@@ -3,14 +3,36 @@
 
 // when opened display current month, day, year
 $("#currentDay").text(moment().format('MM/DD/YY'));
-console.log(currentDay);
+// console.log(currentDay);
 
+// color code blocks for past, present, or future
+
+$(document).ready(function() {
+    colorChange();
+    renderText();
+})
+
+function colorChange() {
+    var currentTime = moment().hours();
+    console.log(currentTime);
+};
+// function that tells blocks whether they are past, present or future
+$(".input").each(function() {
+    var scheduledTime = parseInt($(this).attr("id"));
+    console.log(scheduledTime);
+})
 
 // ability to add tasks
+function renderText() {
 
-// create task function
+}
+
+
 
 // save task function
+$(".saveBtn").click(function() {
+
+}) 
 
 
 
